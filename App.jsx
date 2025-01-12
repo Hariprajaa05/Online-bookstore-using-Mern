cd541a760466094ccd685666f58e849a07d081dd
Hariprajaa B <953622104030@ritrjpm.ac.in>
2025-01-12 23:03:44 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -2,14 +2,12 @@ import Frontpage from "./Frontpage.jsx";
 import Login from './Login.jsx';
 import Signup from './signup.jsx';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Register from "./Register.jsx";
 import Profile from "./Profile.jsx";
-import SavedBooksPage from './SavedBooksPage.jsx';
 import Audiobooks from "./Audiobooks";
 import GenreBooks from './GenreBooks.jsx';
 import BookDetails from './BookDetails.jsx'; 
 import EbooksPage from "./EbooksPage";
-
+import BookStuff from "./BookStuff";
 
 function App() {
   return (
@@ -17,6 +15,7 @@ function App() {
       <div>
         <Routes>
           <Route path="/" element={<Frontpage />} />
+          <Route path="/book-details" element={<BookStuff />} />
           <Route path="/audio" element={<Audiobooks />} />
           <Route path="/genre/:genre" element={<GenreBooks />} />
           <Route path="/book/:id" element={<BookDetails />} />
@@ -24,9 +23,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />}/>
           <Route path="/profile" element={<Profile />} />
-          <Route path="/saved-books" element={<SavedBooksPage />}
           
-           /> 
         </Routes>
       </div>
     </Router>
